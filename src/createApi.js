2fa8ecb7082963152a30e8bb32415613bc9ec1a0
Jazmín Penayo <jazminpenayo@gmail.com>
2022-01-23 18:30:21 -0300

    createApi:  Add form to create new sources

---
+++ src/createApi.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+export default class CreateApi extends Component {
+
+
+  render() {
+    return (
+      <div>
+        <h2>REGISTRAR API KEY</h2>
+        <br />
+        <input />  <br />
+        <button>Registrar</button>
+        <br /> <br />
+      </div>
+    );
+  }
+}
